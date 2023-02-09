580f5694858d576e11238e0ff8a5e8ffe14085bb
TheMike1972 <TheMike1972@github.com>
2023-02-09 18:02:22 +0100

    Michaël Biezin WD FT Paris JAN23 CSS work in progress

--- app.js
+++ app.js
@@ -14,12 +14,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(`${__dirname}/views/partials`)
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {navbar: true});
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+punkAPI
+  .getBeers() 
+  .then(beersList => {
+    res.render('beers', {beersList})
+  }) 
+  .catch(error => console.log(error));
+})
+ 
+app.get('/beers/beer:id', (req, res) => {
+  punkAPI
+  .getBeer(req.params.id)
+  .then(beerId => {
+    res.render('beer', {
+      beer: beerId
+    })
+  })
+})
+
+app.get('/random-beer', (req, res) => {
+punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {responseFromAPI})
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('*', (req, res) => {
+  res.render('server-error');
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    return console.error(err)
+  }
+console.log('🏃‍ on port 3000');
+})
